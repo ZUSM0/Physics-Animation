5f562601afb05afe992b7d4fc714ff20296d33c3
João Manoel <joaomanoeldafe2003@gmail.com>
2025-03-27 11:02:23 -0300

    feat: partial animation mru

--- mru/carro.py
+++ mru/carro.py
@@ -1,18 +1,63 @@
 from manim import *
 
 class exemplos(Scene):
-    def construct(self):
-        titulodados = Tex("Dados: ").scale(1.5).to_edge(UP*1.5+RIGHT*4.3)
-        dado1 =    MathTex(r"\Delta S = 200 km").scale(1.3).to_edge(UP*3+RIGHT*0.7)
-        dado2 = MathTex(r"\Delta t = 4 h").scale(1.3).to_edge(UP*4.5+RIGHT*3.45)
+    def construct(self):        
+        titulodados = Tex("Dados: ").scale(1.5).to_edge(UP*1.5+RIGHT*3.9)
+        dado1 = MathTex(r"\Delta S = 200 km").scale(1.3).shift(ORIGIN)
+        dado2 = MathTex(r"\Delta t = 4 h").scale(1.3).shift(ORIGIN)
+        
+        formula1 = MathTex(r"v_{m} = \frac{200}{4}").scale(1.6)
+        formula2 = MathTex(r"v_{m} = 50 km/h").scale(1.6)
+
+        # Colocar carro no inicio da animação!
+        self.play(Write(titulodados))
+        self.play(Write(dado1), run_time=1.75)
+        self.play(dado1.animate.move_to(UP*2+RIGHT*5), run_time=2)
+        self.play(Write(dado2),run_time=1)
+        self.play(dado2.animate.move_to(UP*1.4+RIGHT*4.3), run_time=2)
+
+        self.wait(2)
+        self.play(Write(formula1), run_time=2)
+        self.wait(2)
+        self.play(TransformMatchingShapes(formula1, formula2), run_time=2)
+        self.wait(3)
+
+        # Ver como remover dado1, dado2, formula2  
+        
+        # Parte do ciclista 
+        dadoBic1 =    MathTex(r"\Delta S = 2 km").scale(1.3).shift(ORIGIN)
+        dadoBic2 = MathTex(r"\Delta t = 5 min").scale(1.3).shift(ORIGIN+DOWN*0.5)   
+        
+        dadoBic12 = MathTex(r"\Delta S = 2000 m").scale(1.3)
+        dadoBic22 = MathTex(r"\Delta t = 300 s").scale(1.3).shift(ORIGIN+DOWN*0.5)  
+
+        self.play(Write(dadoBic1))
+        self.play(Write(dadoBic2))
+        self.play(TransformMatchingShapes(dadoBic1, dadoBic12))
+        self.play(TransformMatchingShapes(dadoBic2, dadoBic22))
+
+        formulabic1 = MathTex(r"v_{m} = \frac{2000}{300}").scale(1.6)
+        formulabic2 = MathTex(r"v_{m} = 6.67 m/hs").scale(1.6)
+
+        self.play(dadoBic12.animate.move_to(UP*2+RIGHT*5), run_time=2)
+        self.play(dadoBic22.animate.move_to(UP*1.4+RIGHT*4.3), run_time=2)
+
+        self.wait(2)
+        self.play(Write(formulabic1), run_time=2)
+        self.wait(2)
+        self.play(TransformMatchingShapes(formulabic1, formulabic2), run_time=2)
+        self.wait(3)
+
+        # Parte da pessoa
+        dadoPes1 =    MathTex(r"\Delta S = 10 m").scale(1.3).to_edge(UP*3+RIGHT*0.7)
+        dadoPes2 = MathTex(r"\Delta t = 4 s").scale(1.3).to_edge(UP*4.5+RIGHT*3.45)
        
-        dados = VGroup(titulodados, dado1, dado2)
+        formulaPes1 = MathTex(r"v_{m} = \frac{10}{4}")
+        formulaPes2 = MathTex(r"v_{m} = 2.5 m/s")
 
-        formula = MathTex(r"v_{m} = \frac{\Delta x}{\Delta t} ").scale(1.6).shift(UP*1+LEFT*1)
+        formulaPes3 = MathTex(r"v_{m} = 2.5 \cdot 3.6")
+        formulaPes4 = MathTex(r"v_{m} = 9 km/h")
 
-        self.add(titulodados)
-        self.add(dado1)
-        self.add(dado2)
-
-        self.add(formula)
+        formulaPes5 = MathTex(r"v_{m} = \frac{9}{3.6}")
+        formulaPes6 = MathTex(r"v_{m} = 2.5 m/s")
 
