c69729c7904fda65211d1a64a04cc7d9d53e63f0
João Manoel <joaomanoeldafe2003@gmail.com>
2025-04-02 10:40:23 -0300

    feat: mru bike animation

--- mru/bicicleta.py
+++ mru/bicicleta.py
@@ -3,27 +3,39 @@ from manim import *
 class bicicleta(Scene):
   def construct(self):
     #Parte do ciclista 
+    titulodados = Tex("Dados: ").scale(1.5).to_edge(UP*1.5+RIGHT*3.9)
+    dadoBic1 = MathTex(r"\Delta S = 2 km").scale(1.3).shift(DOWN*3.1+LEFT*0.6)
+    dadoBic2 = MathTex(r"\Delta t = 5 min").scale(1.3).shift(ORIGIN+LEFT*0.6) 
     
-    dadoBic1 = MathTex(r"\Delta S = 2 km").scale(1.3).shift(ORIGIN)
-    dadoBic2 = MathTex(r"\Delta t = 5 min").scale(1.3).shift(ORIGIN+DOWN*1)   
+    bike = ImageMobject("imagens\image.png")
+    vector = Vector([8, 0]).shift(DOWN*2.5+LEFT*5)
+    bike.scale(0.35).shift(DOWN*1.5+LEFT*5)
+        
+    self.add(titulodados)  
     
-    dadoBic12 = MathTex(r"\Delta S = 2000 m").scale(1.3).shift(ORIGIN)
-    dadoBic22 = MathTex(r"\Delta t = 300 s").scale(1.3).shift(ORIGIN+DOWN*1)  
+    self.play(FadeIn(bike), Write(vector), run_time=1.5)
+    self.wait(1.5)
+    
+    dadoBic12 = MathTex(r"\Delta S = 2000 m").scale(1.3).shift(DOWN*3.1+LEFT*0.6)
+    dadoBic22 = MathTex(r"\Delta t = 300 s").scale(1.3).shift(ORIGIN+LEFT*0.6)  
 
     self.play(Write(dadoBic1), run_time=2)
     self.play(Write(dadoBic2), run_time=2)
     self.wait(3)
-    self.play(TransformMatchingShapes(dadoBic1, dadoBic12), run_time=2)
+    
+    self.play(bike.animate.move_to(DOWN*1.5), TransformMatchingShapes(dadoBic1, dadoBic12), run_time=2)
+    self.wait(1)
     self.play(TransformMatchingShapes(dadoBic2, dadoBic22), run_time=2)
+    
+    self.wait(2)
 
+    self.play(dadoBic12.animate.move_to(UP*2+RIGHT*5), dadoBic22.animate.move_to(UP*1.4+RIGHT*4.6), run_time=2)
+    
     formulabic1 = MathTex(r"v_{m} = \frac{2000}{300}").scale(1.6).shift(ORIGIN+UP*1+LEFT*1.5)
     formulabic2 = MathTex(r"v_{m} = 6.67 m/s").scale(1.6).shift(ORIGIN+UP*1+LEFT*1.5)
 
-    self.play(dadoBic12.animate.move_to(UP*2+RIGHT*5), run_time=2)
-    self.play(dadoBic22.animate.move_to(UP*1.4+RIGHT*4.6), run_time=2)
-
     self.wait(2)
-    self.play(Write(formulabic1), run_time=3)
+    self.play(Write(formulabic1), run_time=2.75)
     self.wait(2)
     self.play(TransformMatchingShapes(formulabic1, formulabic2), run_time=2)
     self.wait(3)
--- mru/pessoa.py
+++ mru/pessoa.py
@@ -3,20 +3,30 @@ from manim import *
 class pessoa(Scene):
   def construct(self):
     # Parte da pessoa
+    titulodados = Tex("Dados: ").scale(1.5).to_edge(UP*1.5+RIGHT*3.9)
     dadoPes1 = MathTex(r"\Delta S = 10 m").scale(1.3).shift(ORIGIN+UP*1.5)
     dadoPes2 = MathTex(r"\Delta t = 4 s").scale(1.3).shift(ORIGIN+UP*0.7)
 
-    self.play(Write(dadoPes1), run_time=2)
-    self.play(Write(dadoPes2), run_time=2)
+    person = ImageMobject("imagens\pessoa.jpg")
+    vector = Vector([8, 0]).shift(DOWN*2.5+LEFT*5)
+    person.scale(0.5).shift(DOWN*1.5+LEFT*5)
+        
+    self.add(titulodados)
+        
+    self.play(FadeIn(person), Write(vector), run_time=1.5)
+    self.wait(1.5)
 
-    self.play(dadoPes1.animate.move_to(UP*2+RIGHT*4.7), run_time=2)
-    self.play(dadoPes2.animate.move_to(UP*1.4+RIGHT*4.3), run_time=2)
-    self.wait(2)
+    self.play( Write(dadoPes1.shift(DOWN*5), run_time=1.75), person.animate.move_to(DOWN*1.5), run_time=2)
+    self.play(Write(dadoPes2.shift(DOWN*0.5+LEFT*5)), run_time=1.75)
+
+    self.play(dadoPes1.animate.move_to(UP*2+RIGHT*4.7), dadoPes2.animate.move_to(UP*1.4+RIGHT*4.3), run_time=2)
+    self.wait(0.4)
 
     formulaPes1 = MathTex(r"v_{m} = \frac{10}{4}").scale(1.6).shift(ORIGIN+UP*1+LEFT*1.5)
     formulaPes2 = MathTex(r"v_{m} = 2.5 m/s").scale(1.6).shift(ORIGIN+UP*1+LEFT*1.5)
 
     self.play(Write(formulaPes1), run_time=2)
+    self.wait(1.75)
     self.play(TransformMatchingShapes(formulaPes1, formulaPes2), run_time=2)
     self.wait(2)
 
